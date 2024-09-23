bb7d5e39b6391bdedb19ee3ddf8e948fd580a05d
Marcus Rådell <marcus@radell.net>
2024-09-23 18:36:54 +0200

    setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   content: [
@@ -6,14 +8,9 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  daisyui: {
+    themes: ["night"],
   },
-  plugins: [],
+  plugins: [typography, daisyui],
 };
 export default config;
